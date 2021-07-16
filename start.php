eecca89c833c17a5df7ebb92640f11f7a62c9baf
kovey <kovey@vip.qq.com>
2021-07-16 13:31:44 +0800

    add node

--- start.php
+++ start.php
@@ -16,12 +16,18 @@ date_default_timezone_set('Asia/shanghai');
 
 use Kovey\Cronjob\Run;
 use Kovey\Library\Config\Manager;
+use Kovey\Library\Util\Json;
+use Kovey\Cronjob\Util\Debug;
 
 Manager::init(APPLICATION_PATH . '/conf');
 
 if (!is_dir(APPLICATION_PATH . '/lock')) {
     mkdir(APPLICATION_PATH . '/lock', 0777, true);
 }
+
+$config = Json::decode(file_get_contents(APPLICATION_PATH . '/conf/cron.json'));
+
+Debug::setNodeName($config['node_name']);
 
 $run = new Run(APPLICATION_PATH . '/lock', 'Cron');
 $run->loop();
